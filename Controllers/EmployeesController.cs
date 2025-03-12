5885401194022ac8c1f65fa360ddfae63c9361f9
prafulnimje <prafulnimje1999@gmail.com>
2025-03-12 15:28:47 +0530

    Crud operation

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeAdminPortal.Controllers
@@ -7,5 +10,45 @@ namespace EmployeeAdminPortal.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //Get All Employees
+        [HttpGet]
+        public IActionResult GetAllEmployee()
+        {
+            return Ok(dbContext.Employees.ToList());
+        }
+
+        //Get Employee by ID
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var emp = dbContext.Employees.Find(id);
+            if (emp == null) { return NotFound(); }
+            return Ok(emp);
+        }
+        //Post
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto) 
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+            
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
     }
 }
